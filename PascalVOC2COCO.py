35e8991bb5f8f4ffa87583c4efbf87d084823b36
shliu <liushuai@tunicorn.cn>
2018-10-18 16:13:23 +0800

    parse xml

--- PascalVOC2COCO.py
+++ PascalVOC2COCO.py
@@ -4,13 +4,179 @@
 import argparse
 import json
 #import matplotlib.pyplot as plt
-import skimage.io as io
+#import skimage.io as io
 import cv2
 #from labelme import utils
 import numpy as np
 import glob
 #import PIL.Image
+import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import ElementTree,Element
+from xml.etree.ElementTree import SubElement
 import os,sys
+
+def read_xml(in_path): 
+    tree = ElementTree()
+    tree.parse(in_path)
+    return tree  
+  
+def write_xml(tree, out_path):  
+    tree.write(out_path, encoding="utf-8",xml_declaration=True)  
+  
+def if_match(node, kv_map):  
+    for key in kv_map:  
+        if node.get(key) != kv_map.get(key):  
+            return False  
+    return True  
+  
+#---------------search -----  
+  
+def find_nodes(tree, path):  
+    return tree.findall(path)  
+  
+  
+def get_node_by_keyvalue(nodelist, kv_map):   
+    result_nodes = []  
+    for node in nodelist:  
+        if if_match(node, kv_map):  
+            result_nodes.append(node)  
+    return result_nodes  
+  
+#---------------change -----  
+  
+def change_node_properties(nodelist, kv_map, is_delete=False):    
+    for node in nodelist:  
+        for key in kv_map:  
+            if is_delete:   
+                if key in node.attrib:  
+                    del node.attrib[key]  
+            else:  
+                node.set(key, kv_map.get(key))  
+              
+def change_node_text(nodelist, text, is_add=False, is_delete=False):  
+    for node in nodelist:  
+        if is_add:  
+            node.text += text  
+        elif is_delete:  
+            node.text = ""  
+        else:  
+            node.text = text  
+              
+def create_node(tag, property_map, content):  
+    element = Element(tag, property_map)  
+    element.text = content  
+    return element  
+          
+def add_child_node(nodelist, element):  
+    for node in nodelist:  
+        node.append(element)  
+          
+def del_node_by_tagkeyvalue(nodelist, tag, kv_map):   
+    for parent_node in nodelist:  
+        children = parent_node.getchildren()  
+        for child in children:  
+            if child.tag == tag and if_match(child, kv_map):  
+                parent_node.remove(child)  
+                          
+
+def parse_xml_WH(filename):
+    """ Parse a PASCAL VOC xml file """
+    tree = ET.parse(filename)
+    obj_size = tree.find('size')
+    obj_struct = {}
+    obj_struct['width'] = (obj_size.find('width').text)
+    obj_struct['height'] = (obj_size.find('height').text)
+    obj_struct['depth'] = (obj_size.find('depth').text)  
+
+def image_check(annopath,
+             imagesetfile,
+             cachedir,
+             jpegpath):
+    print("begin")
+    # first load gt
+    if not os.path.isdir(cachedir):
+        os.mkdir(cachedir)
+    cachefile = os.path.join(cachedir, 'check_image_annots.pkl')
+    # read list of images
+    with open(imagesetfile, 'r') as f:
+        lines = f.readlines()
+    imagenames = [x.strip() for x in lines]
+
+    if not os.path.isfile(cachefile):
+        # load annots
+        recs = {}
+        for i, imagename in enumerate(imagenames):
+            recs[imagename] = parse_xml_WH(annopath.format(imagename))
+            if i % 100 == 0:
+                print('Reading annotation for {:d}/{:d}'.format(
+                    i + 1, len(imagenames)))
+        # save
+        print('Saving cached annotations to {:s}'.format(cachefile))
+        # with open(cachefile, 'w') as f:
+        #     cPickle.dump(recs, f)
+    else:
+        print("load")
+        # with open(cachefile, 'r') as f:
+        #     recs = cPickle.load(f)
+
+    # extract gt objects for this class
+    class_recs = {}
+    npos = 0
+    ballsame = True
+    for imagename in imagenames:
+        im = cv2.imread(jpegpath.format(imagename))
+        #sp = im.shape
+        height = im.shape[0]
+        width = im.shape[1]
+        depth = im.shape[2]
+        if depth != 3 :
+             print("depet wrong",imagename)
+        # print type(recs[imagename]['width']) ,type(width)
+        if ((int(recs[imagename]['width'])  != width)or (int(recs[imagename]['height']) != height) or (int(recs[imagename]['depth']) != depth)) :
+            print(imagename,recs[imagename]['width'],width, recs[imagename]['height'],height,recs[imagename]['depth'],depth)
+            ballsame=False
+            # cv2.namedWindow(imagename)
+            # cv2.imshow(imagename, im)
+            # cv2.waitKey (0)
+    print("==========")
+    if ballsame:
+        print("==========the anotationes and images all match ")
+    else:
+        print("==========please check the differences")
+    print("end")
+
+def parse_xml(filename):
+    """ Parse a PASCAL VOC xml file """
+    tree = ET.parse(filename)
+    # tree=ElementTree()
+    # tree.parse(filename)
+
+    baseInfo={}
+    if tree.find('folder'):
+        baseInfo['folder'] = tree.find('folder').text
+    baseInfo['filename'] = tree.find('filename').text
+    baseInfo['path'] = tree.find('path').text
+    baseInfo['source/database'] = tree.find('source/database').text
+    #tree.find('database')
+    baseInfo['size/width'] = tree.find('size/width').text
+    baseInfo['size/height'] = tree.find('size/height').text
+    baseInfo['size/depth'] = tree.find('size/depth').text
+    baseInfo['segmented'] = tree.find('segmented').text
+    objects = []
+    for obj in tree.findall('object'):
+        obj_struct = {}
+        obj_struct['name'] = obj.find('name').text
+        obj_struct['pose'] = obj.find('pose').text
+        obj_struct['truncated'] = obj.find('truncated').text #remove int()
+        obj_struct['difficult'] = obj.find('difficult').text #remove int()
+        bbox = obj.find('bndbox')
+        obj_struct['bbox'] = [int(bbox.find('xmin').text),
+                              int(bbox.find('ymin').text),
+                              int(bbox.find('xmax').text),
+                              int(bbox.find('ymax').text)]
+        objects.append(obj_struct)
+
+    return baseInfo,objects
 
 
 class PascalVOC2coco(object):
@@ -56,12 +222,15 @@ class PascalVOC2coco(object):
             if self.supercategory not in self.label:
                 self.categories.append(self.categorie())
                 self.label.append(self.supercategory)
-        print imagesetfile,"label = ",i
+        print(imagesetfile,"label = ",i)
         return tuple(sku_list)
 
-    def data_transfer_2(self):
-        txt_dir="/data/tiannuodata/nestle4goods/nestle4goodsproj1/ImageSets/Main/"
-        outdir="/home//liushuai/work/light_head_rcnn/data/MSCOCO/odformat/"
+    def data_transfer_3(self):
+        # notconvert_list=[]
+        # notconvert_list.append("budweiser47401")
+        # notconvert_list.append("budweiser47379")
+        txt_dir="/data/liushuai/baiwei/baiweiproj329//ImageSets/Main/"
+        outdir="//data/liushuai/baiwei/baiweiproj329//odformat/"
         out_trainfile=outdir+"coco_trainvalmini.odgt"
         out_valfile=outdir+"coco_minival2014.odgt"
         train_orgpath=txt_dir+"/trainval.txt"
@@ -92,12 +261,166 @@ class PascalVOC2coco(object):
 
         trainstring_list=[]
         teststring_list=[]
+        print("xml len:",len(self.xml))
         for num, json_file in enumerate(self.xml):
 
             basename=os.path.basename(json_file)
             basename=os.path.splitext(basename)[0]
+            # if basename in notconvert_list:
+            #     continue
             # 进度输出
-            sys.stdout.write('\r>> Converting image %d/%d %s' % (
+            sys.stdout.write('\r>> Converting image %d/%d %s ' % (
+                num + 1, len(self.xml),basename))
+            sys.stdout.flush()
+
+            self.json_file = json_file
+            self.num = num
+            path = os.path.dirname(self.json_file)
+            path = os.path.dirname(path)
+            # path=os.path.split(self.json_file)[0]
+            # path=os.path.split(path)[0]
+            #####################################################
+            baseInfo,objects = parse_xml(json_file)
+            writelin="{\"gtboxes\":["
+            obj_path = glob.glob(os.path.join(path, 'SegmentationObject', '*.png'))
+            bboxwrite=""
+            bfirst=True
+            for objbbox in objects:
+                if bfirst==False:
+                        bboxwrite=bboxwrite+","
+                else:
+                    bfirst=False
+                bboxwrite=bboxwrite+"{\"bbox\": "
+                bboxwrite=bboxwrite+str([objbbox['bbox'][0], objbbox['bbox'][1], objbbox['bbox'][2] - objbbox['bbox'][0], objbbox['bbox'][3] - objbbox['bbox'][1]])
+                bboxwrite=bboxwrite+", \"occ\": 0,"
+                bboxwrite=bboxwrite+"\"tag\": \""+objbbox['name']
+                bboxwrite=bboxwrite+"\",\"extra\": {\"ignore\": 0}"
+                bboxwrite=bboxwrite+"}"
+            writelin=writelin+bboxwrite+"],"
+            fpath="\"fpath\": \"/val2014/"+basename+".jpg\", \"dbName\": \"COCO\", \"dbInfo\": {\"vID\": \"COCO_trainval2014_womini\", \"frameID\": -1}, \"width\": "
+            fpath=fpath+baseInfo['size/width']+", \"height\": "+baseInfo['size/height']+",\"ID\": \""+basename+".jpg\""
+            writelin=writelin+fpath+"}\n"
+            #####################################################
+            # writelin="{\"gtboxes\":["
+            # obj_path = glob.glob(os.path.join(path, 'SegmentationObject', '*.png'))
+            # bboxwrite=""
+            # bfirst=True
+            # with open(json_file, 'r') as fp:
+            #     bobject_in=False
+            #     for p in fp:
+            #         # if 'folder' in p:
+            #         #     folder =p.split('>')[1].split('<')[0]
+            #         if 'filename' in p:
+            #             path=os.path.split(self.json_file)[0]
+            #             self.filen_ame = basename#p.split('>')[1].split('<')[0]
+
+            #             self.path = os.path.join(path, 'SegmentationObject', self.filen_ame.split('.')[0] + '.png')
+
+
+            #         if 'width' in p:
+            #             sssssssa=p.split('>')[1].split('<')[0]
+            #             # if basename=="budweiser47401":
+            #             #     print("===  ",sssssssa)
+            #             self.width = int(sssssssa)
+            #         if 'height' in p:
+            #             self.height = int(p.split('>')[1].split('<')[0])
+
+            #             self.images.append(self.image())
+
+            #         if '</object>' in p:
+            #             bobject_in=False
+            #         if '<object>' in p:
+            #             bobject_in=True
+            #         if bobject_in==True and 'name' in p:
+            #             self.supercategory = p.split('>')[1].split('<')[0]
+            #             if self.supercategory not in self.label:
+            #                 print("======================================error")
+            #         if bobject_in==True and '<bndbox>' in p:
+            #             d = [next(fp).split('>')[1].split('<')[0] for _ in range(4)]
+            #             # 边界框
+            #             x1 = int(d[-4]);
+            #             y1 = int(d[-3]);
+            #             x2 = int(d[-2]);
+            #             y2 = int(d[-1])
+            #             self.rectangle = [x1, y1, x2, y2]
+            #             self.bbox = [x1, y1, x2 - x1, y2 - y1]  # COCO 对应格式[x,y,w,h]
+            #             self.annotations.append(self.annotation())
+            #             self.annID += 1
+            #             if bfirst==False:
+            #                     bboxwrite=bboxwrite+","
+            #             else:
+            #                 bfirst=False
+            #             bboxwrite=bboxwrite+"{\"bbox\": "
+            #             bboxwrite=bboxwrite+str(self.bbox)
+            #             bboxwrite=bboxwrite+", \"occ\": 0,"
+            #             bboxwrite=bboxwrite+"\"tag\": \""+self.supercategory
+            #             bboxwrite=bboxwrite+"\",\"extra\": {\"ignore\": 0}"
+            #             bboxwrite=bboxwrite+"}"
+
+            # writelin=writelin+bboxwrite+"],"
+            # fpath="\"fpath\": \"/val2014/"+basename+".jpg\", \"dbName\": \"COCO\", \"dbInfo\": {\"vID\": \"COCO_trainval2014_womini\", \"frameID\": -1}, \"width\": "
+            # fpath=fpath+str(self.width)+", \"height\": "+str(self.height)+",\"ID\": \""+basename+".jpg\""
+            # writelin=writelin+fpath+"}\n"
+            if basename in train_list:
+                for zzz in train_list:
+                    if basename == zzz:
+                        trainstring_list.append(writelin)
+            elif basename in test_list:
+                for zzz in test_list:
+                    if basename == zzz:
+                        teststring_list.append(writelin)
+        with open(out_trainfile, 'w') as fout_trainfile:
+            fout_trainfile.writelines(trainstring_list)
+        with open(out_valfile, 'w') as fout_valfile:
+            fout_valfile.writelines(teststring_list)
+        sys.stdout.write('\n')
+        sys.stdout.flush()
+
+    def data_transfer_2(self):
+        notconvert_list=[]
+        notconvert_list.append("budweiser47401")
+        notconvert_list.append("budweiser47379")
+        txt_dir="/data/liushuai/baiwei/baiweiproj329//ImageSets/Main/"
+        outdir="//data/liushuai/baiwei/baiweiproj329//odformat/"
+        out_trainfile=outdir+"coco_trainvalmini.odgt"
+        out_valfile=outdir+"coco_minival2014.odgt"
+        train_orgpath=txt_dir+"/trainval.txt"
+        with open(train_orgpath, 'r') as f:
+            lines_semi = f.readlines()
+
+        train_list = []
+        for line in lines_semi:
+            x=line.strip()
+            x=line.strip('\n')
+            x=line.strip('\r')
+            x=line.strip('\n')
+            x=line.strip()
+            train_list.append(x)
+
+        test_orgpath=txt_dir+"/test.txt"
+        with open(test_orgpath, 'r') as f:
+            lines_semi = f.readlines()
+
+        test_list = []
+        for line in lines_semi:
+            x=line.strip()
+            x=line.strip('\n')
+            x=line.strip('\r')
+            x=line.strip('\n')
+            x=line.strip()
+            test_list.append(x)
+
+        trainstring_list=[]
+        teststring_list=[]
+        print("xml len:",len(self.xml))
+        for num, json_file in enumerate(self.xml):
+
+            basename=os.path.basename(json_file)
+            basename=os.path.splitext(basename)[0]
+            if basename in notconvert_list:
+                continue
+            # 进度输出
+            sys.stdout.write('\r>> Converting image %d/%d %s ' % (
                 num + 1, len(self.xml),basename))
             sys.stdout.flush()
 
@@ -124,7 +447,10 @@ class PascalVOC2coco(object):
 
 
                     if 'width' in p:
-                        self.width = int(p.split('>')[1].split('<')[0])
+                        sssssssa=p.split('>')[1].split('<')[0]
+                        # if basename=="budweiser47401":
+                        #     print("===  ",sssssssa)
+                        self.width = int(sssssssa)
                     if 'height' in p:
                         self.height = int(p.split('>')[1].split('<')[0])
 
@@ -137,7 +463,7 @@ class PascalVOC2coco(object):
                     if bobject_in==True and 'name' in p:
                         self.supercategory = p.split('>')[1].split('<')[0]
                         if self.supercategory not in self.label:
-                            print "======================================error"
+                            print("======================================error")
                     if bobject_in==True and '<bndbox>' in p:
                         d = [next(fp).split('>')[1].split('<')[0] for _ in range(4)]
                         # 边界框
@@ -245,7 +571,7 @@ class PascalVOC2coco(object):
                         if self.supercategory not in self.label:
                             # self.categories.append(self.categorie())
                             # self.label.append(self.supercategory)
-                            print "======================================error"
+                            print("======================================error")
                     if bobject_in==True and '<bndbox>' in p:
                         d = [next(fp).split('>')[1].split('<')[0] for _ in range(4)]
                         # 边界框
@@ -391,8 +717,8 @@ class PascalVOC2coco(object):
         return data_coco
 
     def save_json(self):
-        self.make_labelmap("/data/tiannuodata/nestle4goods/nestle4goodsproj1/","skufile.txt")
-        self.data_transfer_2()
+        self.make_labelmap("/data/liushuai/baiwei/baiweiproj329//","skufile.txt")
+        self.data_transfer_3()
         self.data_coco = self.data2coco()
         # 保存json文件
         json.dump(self.data_coco, open(self.save_json_path, 'w'), indent=4)  # indent=4 更加美观显示
@@ -432,7 +758,7 @@ def txt_2_odformattrain_val(txt_dir,json_file,outdir):
         for trian in train_list:
             for line in datas['images']:
                 if trian==line['file_name']:
-                    print trian
+                    print(trian)
                     writelin="{\"gtboxes\":["
                     # print line['id']
                     # print line['file_name']
@@ -472,7 +798,7 @@ def txt_2_odformattrain_val(txt_dir,json_file,outdir):
         for test in test_list:
             for line in datas['images']:
                 if test==line['file_name']:
-                    print test
+                    print(test)
                     writelin="{\"gtboxes\":["
                     # print line['id']
                     # print line['file_name']
@@ -510,14 +836,14 @@ def txt_2_odformattrain_val(txt_dir,json_file,outdir):
                     fout_valfile.write(writelin)
 
 if __name__ == "__main__":
-    txt_list="/data/tiannuodata/nestle4goods/nestle4goodsproj1/ImageSets/Main/"
-    json_file='/data/tiannuodata/nestle4goods/nestle4goodsproj1/new.json'
-    json_file="/home/liushuai/work/light_head_rcnn/data/MSCOCO/instances_minival2014.json"
+    txt_list="/data/liushuai/baiwei/baiweiproj329//ImageSets/Main/"
+    json_file='/data/liushuai/baiwei/baiweiproj329//new.json'
+    #json_file="/home/liushuai/work/light_head_rcnn/data/MSCOCO/instances_minival2014.json"
     #json_file='/data/tiannuodata/nestle4goods/nestle4goodsproj1/2.json'
-    outdir="/home/liushuai//work/light_head_rcnn/data/MSCOCO/odformat/" #coco_minival2014.odgt coco_trainvalmini.odgt
+    outdir="/data/liushuai/baiwei/baiweiproj329//odformat/" #coco_minival2014.odgt coco_trainvalmini.odgt
     #txt_2_odformattrain_val(txt_list,json_file,outdir)
-    xml_file = glob.glob('/data/tiannuodata/nestle4goods/nestle4goodsproj1/Annotations/*.xml')
-    PascalVOC2coco(xml_file, '/data/tiannuodata/nestle4goods/nestle4goodsproj1/new.json')
+    xml_file = glob.glob('/data/liushuai/baiwei/baiweiproj329/Annotations/*.xml')
+    PascalVOC2coco(xml_file, '/data/liushuai/baiwei/baiweiproj329//new.json')
 
     # #=========================================================================test
     # json_file='/data/tiannuodata/nestle4goods/nestle4goodsproj1/new.json'
